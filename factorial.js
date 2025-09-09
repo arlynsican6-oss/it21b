38219401e37a2fd71535c345b7a937fa7122976a
arlynsican6-oss <arlynsican6@gmail.com>
2025-09-09 12:06:27 +0800

    added my work factorial

---
+++ factorial.js
@@ -0,0 +1,18 @@
+//Console Input 
+const readline =require("readline");
+
+const rl = readline.createInterface({
+    inout:Process.stdin,
+    output:process.stdout
+})
+
+console.log("-------------------------------");
+console.log("Factorial Application");
+console.log("1. say hello");
+console.log("2. factorial");
+console.log("3. Eixt application");
+console.log("-------------------------------;")
+
+r1.question("enter your choice (1-3):,choice =")
+
+
